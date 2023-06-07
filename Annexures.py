09c49f9f1f37c29650a0fb514b05712135326b57
Purna Sanapala <purnass2121@gmail.com>
2023-06-07 10:31:33 +0530

    first

--- Annexures.py
+++ Annexures.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 # from selenium import webdriver
 from selenium import webdriver
+
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
